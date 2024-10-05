8d3c942b652fbdb643eb4a8cafc236569250d1e5
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-10-05 10:32:46 -0300

    finish home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,18 @@
+import { VStack } from "@chakra-ui/react";
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+
 export default function Home() {
     return (
-        <>
-            <p>Home</p>
-        </>
+        <VStack
+            h={"91vh"}
+            overflow={'hiden'}
+            paddingBottom={'2em'}
+            paddingTop={'1em'}
+            spacing="1rem"
+            w="full"
+        >
+            <Fireworks autorun={{ speed: 1, duration: 4000 }} />
+            <h1>Welcome to the Home Page</h1>
+        </VStack>
     )
 }
