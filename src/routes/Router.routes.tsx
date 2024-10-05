80cad07d3188e3b238d8cce6a4f8e24241d1930f
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-10-05 11:24:22 -0300

    fix(lazy): add loading view to fallback in routers

--- src/routes/Router.routes.tsx
+++ src/routes/Router.routes.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoutes from "./ProtectedRoutes.routes";
+import Loading from "../pages/Loading/Loading";
 
 const Home = lazy(() => import("../pages/Home/Home"));
 const Login = lazy(() => import("../pages/Login/Login"));
@@ -8,12 +9,13 @@ const Login = lazy(() => import("../pages/Login/Login"));
 export default function Router () {
     return (
         <HashRouter>
-            <Suspense fallback={<p>Loading...</p>}>
+            <Suspense fallback={<Loading />}>
                 <Routes>
                     <Route element={<ProtectedRoutes />}>
                         <Route path="/" element={<Home />} />
                     </Route>
                     <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<Login />} />
                 </Routes>
             </Suspense>
         </HashRouter>
